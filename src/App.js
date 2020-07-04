ac14a73c8ffa29b102e469c07fc8456192bf5c5d
Gerardo Castillo <gerrcass@gmail.com>
2020-07-04 19:12:15 -0400

    implementing code splitting using high order components. It's another way to do the same thing but maybe more cleaner. See the litle flash when click on Page2/Page3 button the first time, it's actually triggering an HTTP GET request to the server to get that js chunk file containing the specific part of the javascript... this is code splitting in action instead of getting the whole bundled file in the initial HTTP request

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import React from "react";
 import "./App.css";
 
 import Page1 from "./PageOne";
-/* import Page2 from "./PageTwo";
-import Page3 from "./PageThree"; */
+import AsyncComponent from "./AsyncComponent";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       route: "page1",
-      component: null,
     };
   }
   onChangeRoute = (route) => {
     // Witout Code Splitting
-    // this.setState({ route: route });
+    this.setState({ route: route });
 
     // With Code Splitting
 
-    if (route === "page1") {
+    /*     if (route === "page1") {
       this.setState({ route: route });
     } else if (route === "page2") {
       import("./PageTwo.js").then((Page2) => {
@@ -29,16 +27,18 @@ class App extends React.Component {
       import("./PageThree.js").then((Page3) => {
         this.setState({ route: route, component: Page3.default });
       });
-    }
+    } */
   };
   render() {
     switch (this.state.route) {
       case "page1":
         return <Page1 onChangeRoute={this.onChangeRoute} />;
       case "page2":
-        return <this.state.component onChangeRoute={this.onChangeRoute} />;
+        const AsyncPage2 = AsyncComponent(() => import("./PageTwo"));
+        return <AsyncPage2 onChangeRoute={this.onChangeRoute} />;
       case "page3":
-        return <this.state.component onChangeRoute={this.onChangeRoute} />;
+        const AsyncPage3 = AsyncComponent(() => import("./PageThree"));
+        return <AsyncPage3 onChangeRoute={this.onChangeRoute} />;
       default:
         return "";
     }
